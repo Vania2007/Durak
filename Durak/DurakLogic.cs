c10f89b0d0e63ea4a719ef2a1f8ede0a32b22b82
Иван Момот <momotis2007@gmail.com>
2023-03-18 12:36:43 +0200

    WhoFirst +

--- Durak/DurakLogic.cs
+++ Durak/DurakLogic.cs
@@ -13,20 +13,80 @@ namespace Durak
         {
             Deck = new CardSet();
             Deck.Full();
-            Player = new Player();
+            
             Table = new CardSet();
             ShowState = showState;
         }
         public Action ShowState { get; set; }
         public CardSet Deck { get; set; }
-        public Player Player { get; set; }
         public List<Player> Players { get; set; }
         public CardSet Table { get; set; }
         public Player Current { get; set; }
+        public Player Attacker { get; set; }
+        public Player Defender { get; set; }
+        public Card Trump { get; set; } = new Card(Deck.LastCard.Rank, Deck.LastCard.Suit);
+        public Player FirstPasser { get; set; }
+
+        private int CardsToBeat { get; set; }
+        enum Mode
+        {
+            Attack,
+            Defend,
+            Toss //підкидання
+        }
+
+
+
+
+
+        //public bool IsGreater(Card tableCard, Card movingCard)
+        //public bool PossibleMove(Card movingCard)
+        //public void PickUp() 
+        //public void GiveUp() defender says «I give up»
+        //public void Pass()
+        //public PlayerNextPlayer()
+        
+        //public void Beat()
+        //public void NewTurn()
+
+
+        public Player WhoFirst()
+        {
+            Card smallestCard;
+            List<Card> trumps = new List<Card>();
+            foreach (var player in Players)
+            {
+                foreach (var card in player.Hand)
+                {
+                    if(card.Suit == Trump.Suit)
+                    {
+                        trumps.Add(card);
+                    }
+                    for (int i = 0; i < trumps.Count; i++)
+                    {
+                        if (card.Suit == Trump.Suit && card.Rank < trumps[i].Rank)
+                        {
+                            smallestCard = card;
+                            
+                        }
+                    }
+                    Attacker = player;
+                    Players[0] = Attacker;
+                    return player;
+                }
+            }
+        }
+        public Player NextAttacker()
+        {
+
+        }
         public void Turn(Card card)
         {
-            if (!Player.Hand.Contains(card)) return;
-            Table.Add(Player.Hand.Pull(card));
+
+            if (!Current.Hand.Contains(card)) return;
+            //if (!PossibleMove(card)) return;
+            Table.Add(Current.Hand.Pull(card));
+            //Current changing
             ShowState();
         }
         public void Start()
@@ -37,18 +97,23 @@ namespace Durak
                 player.Hand.Add(Deck.Deal(6));
                 player.Hand.Sort();
             }
-            foreach (var player in Players)
-            {
-                foreach (var card in player.Hand)
-                {
-                    if (card.Suit == TrumpChoice())
-                        Players[0] = player;
-                }
-            }
+            CardsToBeat = 5;
+            TrumpChoise();
+            Attacker = Players[0];
+            //Who current? Who attacker? Who defender?
+            //foreach (var player in Players)
+            //{
+            //    foreach (var card in player.Hand)
+            //    {
+            //        if (card.Suit == TrumpChoice())
+            //            Players[0] = player;
+            //    }
+            //}
             ShowState();
         }
-        public void Hit()
+        public void DealUp()
         {
+            //whaT IF deck doesn't have enough cards?
             while (Current.Hand.Count < 6)
             {
                 Current.Hand.Add(Deck.Pull());
@@ -57,25 +122,23 @@ namespace Durak
         }
         public void Pass()
         {
+            //next attacker not current
             //передає хід наступному гравцю
-            for (int i = 0; i < Players.Count+1; i++)
-            {
-                Current = Players[i];
-                if (i > Players.Count)
-                    i = 0;
-            }
+            //for (int i = 0; i < Players.Count + 1; i++)
+            //{
+            //    Current = Players[i];
+            //    if (i > Players.Count)
+            //        i = 0;
+            //}
         }
         public void EndOfTheGame()
         {
             //Підбиває підсумки гри, повертає "Дурня"(той, хто програв)
             throw new NotImplementedException();
         }
-        public CardSuit TrumpChoice()
+        public Card TrumpChoise()
         {
-            // і повернути картку, яка показує козир у грі
-            Card trumpCard = Deck[0];
-            return trumpCard.Suit;
+            return Trump = Deck.LastCard;
         }
-
     }
 }
